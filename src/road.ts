2c73e0ec18f8785a00bde7c8f4a9ccf625bb0219
Alexis Brandner <alexis.brandner@enseirb-matmeca.fr>
2024-03-19 17:55:36 +0100

    feat: Immutable-ify road.ts

--- src/road.ts
+++ src/road.ts
@@ -1,8 +1,10 @@
-type Road = { north: boolean, west: boolean, south: boolean, east: boolean};
+import {MapOf, Map} from "immutable";
+
+type Road = MapOf<{ north: boolean, west: boolean, south: boolean, east: boolean}>;
 
 function createRoad() : Road
 {
-    return {north: false, west: false, south: false, east: false};
+    return Map({north: false, west: false, south: false, east: false});
 }
 
 export {Road, createRoad};

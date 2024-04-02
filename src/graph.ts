4ba392638b3a809ae5c489b9a8b8ba70f449931c
Alexis Brandner <alexis.brandner@enseirb-matmeca.fr>
2024-04-02 15:52:00 +0200

    refactor: untabify graph.ts

--- src/graph.ts
+++ src/graph.ts
@@ -18,7 +18,7 @@ function addVertex<T>(graph: Graph<T>, vertex: T): Graph<T>
 function addEdge<T>(graph: Graph<T>, indexV1: number, indexV2: number): Graph<T>
 {
     if (graph.get('adj').get(indexV1, List<number>()).includes(indexV2))
-	return graph; // Edge already exists
+        return graph; // Edge already exists
     
     const newAdj1 = graph.get('adj').get(indexV1, List<number>()).push(indexV2);
     const newGraph = graph.set('adj', graph.get('adj').set(indexV1, newAdj1));
@@ -38,14 +38,14 @@ function getVertexNeighbors<T>(graph: Graph<T>, vertexIndex: number): List<T>
 }
 
 function connexCompRec<T>(graph: Graph<T>,
-			  notVisited: List<T>,
-			  vertexIndex: number,
-			  classes: List<List<T>>,
-			  currentClass: number): MapOf<{notVisited: List<T>, classes: List<List<T>>}>
+                          notVisited: List<T>,
+                          vertexIndex: number,
+                          classes: List<List<T>>,
+                          currentClass: number): MapOf<{notVisited: List<T>, classes: List<List<T>>}>
 {
     const v = graph.get('vertices').get(vertexIndex);
     if (v === undefined)
-	throw new Error("Vertex " + vertexIndex + " is not in graph. This should never happen.");
+        throw new Error("Vertex " + vertexIndex + " is not in graph. This should never happen.");
 
     // Delete the vertex that we're currently visiting.
     const newNotVisited = notVisited.delete(notVisited.indexOf(v));
@@ -58,7 +58,7 @@ function connexCompRec<T>(graph: Graph<T>,
 
     // If no neighbors and no nodes to visit, then return to parent function call.
     if (toVisit.size === 0 && notVisited.size === 0)
-	return Map({notVisited: notVisited, classes: classes});
+        return Map({notVisited: notVisited, classes: classes});
     
     // In case there is nothing to visit, take the next Vertex in notVisited and prepare a new class
     const newToVisit = (toVisit.size === 0) ? List<T>([notVisited.first()]) : toVisit;
@@ -66,18 +66,18 @@ function connexCompRec<T>(graph: Graph<T>,
     const newNewClasses = (toVisit.size === 0) ? newClasses.push(List<T>()) : newClasses;
 
 /*    if (newToVisit === undefined || newToVisit === null)
-	throw new Error("newToVisit is undefined. This should never happen.");*/
+        throw new Error("newToVisit is undefined. This should never happen.");*/
     
     // For each neighbor, do a recursive function call.
     const r = newToVisit.reduce(
-	(acc, val) => connexCompRec(
-	    graph,
-	    acc.get('notVisited'),
-	    graph.get('vertices').indexOf(val),
-	    acc.get('classes'),
-	    newCurrentClass
-	),
-	Map({notVisited: newNotVisited, classes: newNewClasses})
+        (acc, val) => connexCompRec(
+            graph,
+            acc.get('notVisited'),
+            graph.get('vertices').indexOf(val),
+            acc.get('classes'),
+            newCurrentClass
+        ),
+        Map({notVisited: newNotVisited, classes: newNewClasses})
     );
 
     return r;
@@ -87,9 +87,9 @@ function getConnexComponents<T>(graph: Graph<T>): List<List<T>>
 {
     const notVisited: List<T> = graph.get('vertices');
     if (notVisited.size === 0)
-	return List<List<T>>();
+        return List<List<T>>();
     else
-	return connexCompRec<T>(graph, notVisited, 0, List<List<T>>().push(List<T>()), 0).get('classes', List<List<T>>());
+        return connexCompRec<T>(graph, notVisited, 0, List<List<T>>().push(List<T>()), 0).get('classes', List<List<T>>());
 }
 
 export {Graph, initGraph, addVertex, addEdge, getVertices, getVertexNeighbors};

306fdde2d4332a894d32ed19b565840b4ff9943f
Gael Valade <gael.valade@enseirb-matmeca.fr>
2024-04-30 11:51:31 +0200

    [master] feat : new display of board

--- src/common.ts
+++ src/common.ts
@@ -1,3 +1,14 @@
 const nil = undefined;
 
-export {nil};
+const Reset = "\x1b[0m";
+const BlackBg = "\x1b[40m";
+const RedBg = "\x1b[41m";
+const GreenBg = "\x1b[42m";
+const YellowBg = "\x1b[43m";
+const BlueBg = "\x1b[44m";
+const MagentaBg = "\x1b[45m";
+const CyanBg = "\x1b[46m";
+const WhiteBg = "\x1b[47m";
+const GreyBg = "\x1b[100m";
+
+export {nil, Reset, RedBg, GreenBg, GreyBg, BlueBg};
--- src/display.ts
+++ src/display.ts
@@ -1,4 +1,4 @@
-import {nil} from "./common.js";
+import {nil, BlueBg, RedBg, Reset, GreyBg, GreenBg} from "./common.js";
 import * as B from "./board.js";
 import * as T from "./tile.js";
 import * as R from "./road.js";
@@ -34,13 +34,13 @@ function strNeighborhood(n : N.Color) : string
 {
 	switch (n) {
 		case N.Color.Blue:
-			return "b";
+			return BlueBg;
 		case N.Color.Green:
-			return "v";
+			return GreenBg;
 		case N.Color.Grey:
-			return "k";
+			return GreyBg;
 		case N.Color.Red:
-			return "r";
+			return RedBg;
 		default:
 			return "-";
 	}
@@ -48,10 +48,10 @@ function strNeighborhood(n : N.Color) : string
 
 function displayTile(t : T.Tile)
 {
-	console.log(strNeighborhood(t.get("neighborhoods").get("nw")) + strRoad(t.get("roads").get("nw")) + "    " 
-	+ strNeighborhood(t.get("neighborhoods").get("ne")) + strRoad(t.get("roads").get("ne")));
-	console.log(strNeighborhood(t.get("neighborhoods").get("sw")) + strRoad(t.get("roads").get("sw")) + "    " 
-	+ strNeighborhood(t.get("neighborhoods").get("se")) + strRoad(t.get("roads").get("se")) + "\n");
+	console.log(strNeighborhood(t.get("neighborhoods").get("nw")) + strRoad(t.get("roads").get("nw") ) + Reset + "    " 
+	+ strNeighborhood(t.get("neighborhoods").get("ne")) + strRoad(t.get("roads").get("ne")) + Reset);
+	console.log(strNeighborhood(t.get("neighborhoods").get("sw")) + strRoad(t.get("roads").get("sw")) + Reset + "    " 
+	+ strNeighborhood(t.get("neighborhoods").get("se")) + strRoad(t.get("roads").get("se")) + Reset + "\n");
 }
 
 function strXYQuarter(q : B.Quarter) : string
@@ -59,20 +59,54 @@ function strXYQuarter(q : B.Quarter) : string
 	return "Coordonate : (" + q?.get("x") + ", " + q?.get("y") + ")";
 }
 
-function displayQuarter(q : B.Quarter)
+function displayQuarter(q : B.Quarter) : string
 {
-	console.log(strXYQuarter(q));
+	// console.log(strXYQuarter(q));
 	if (q?.get("color") === nil)
 		throw new Error("Pas defini");
-	console.log(strNeighborhood(q?.get("color")));
-	console.log(strRoad(q.get("road")));
+	return strNeighborhood(q.get("color"))+ strRoad(q.get("road")) + Reset;
+	// console.log(strRoad(q.get("road")));
 }
 
-function displayBoard(b : B.Board)
+function displayBoardWithCoordonate(b : B.Board)
 {
 	/* console.log("Etat du board : \n");
 	console.log(b); */
-	b.map((e) => displayQuarter(e));
+	b.map((e) => console.log(displayQuarter(e)));
+}
+
+function displayBoard(board : B.Board)
+{
+	const xmin : number = board.reduce((acc, elem) => (elem === nil)?0:(elem.get("x") < acc)?elem.get("x"):acc, 0);
+	const xmax : number = board.reduce((acc, elem) => (elem === nil)?0:(elem.get("x") > acc)?elem.get("x"):acc, 0);
+	const ymax : number = board.reduce((acc, elem) => (elem === nil)?0:(elem.get("y") > acc)?elem.get("y"):acc, 0);
+	const ymin : number = board.reduce((acc, elem) => (elem === nil)?0:(elem.get("y") < acc)?elem.get("y"):acc, 0);
+	displayColumn(board, ymin, ymax, xmin, xmax);
+}
+
+function displayLine(board : B.Board, y : number, begin : number, end : number) : string
+{
+	if (begin === end+1)
+		return " ";
+	const q : B.Quarter = B.findQuarter(board, begin, y)
+	if (q !== nil)
+		return displayQuarter(q) + displayLine(board, y, begin+1, end);
+	else
+		return "  " + displayLine(board, y, begin+1, end);
+	/* const line = board.filter((quarter) => quarter?.get("y") === y);
+	line.map((elem) => {
+		function dhsj(begin : number, end : number,  )
+	}) */
+}
+
+function displayColumn(board : B.Board, ymin : number, ymax : number, xmin : number, xmax : number)
+{
+	// console.log(" ----------------------- Affichage de la ligne : " + ymax + "-----------------------")
+	if (ymax === ymin-1)
+		return;
+	else 
+		console.log(displayLine(board, ymax, xmin, xmax) + "  " + ymax);
+		return displayColumn(board, ymin, ymax-1, xmin, xmax);
 }
 
 function displayDeck(d : D.Deck) 
@@ -80,4 +114,4 @@ function displayDeck(d : D.Deck)
 	d.map((e) => displayTile(e));
 }
 
-export {strNeighborhood, strRoad, displayTile, displayQuarter, displayBoard, displayDeck};
+export {strNeighborhood, strRoad, displayTile, displayQuarter, displayBoardWithCoordonate, displayDeck, displayBoard};

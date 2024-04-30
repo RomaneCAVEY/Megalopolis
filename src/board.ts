bb75f1b003b5b901f04b55c75246caa43322031e
Alexis Brandner <alexis.brandner@enseirb-matmeca.fr>
2024-04-30 16:26:14 +0200

    [strategy] eslint

--- src/board.ts
+++ src/board.ts
@@ -105,44 +105,44 @@ function findPositionToAddTile(aList: List<number>, board: Board, tile: Tile, ob
     //console.log("\nxRange:", xRange.toArray(), "yRange:", yRange.toArray(), "\n");
 
     const bestCoords: List<number> = xRange.reduce(
-	(acc, x) => {
-	    const bestY: List<number> = yRange.reduce(
-		(acc2, y) => {
-		    if (checkMove(board, x, y) === false) {
-			return acc2;
-		    } else {
-			const nb: Board = placeTile(board, tile, x, y);
-			const rg: G.Graph<Quarter> = buildRoadGraph(board);
-			const ng: G.Graph<Quarter> = buildNeighborhoodGraph(board);
+        (acc, x) => {
+            const bestY: List<number> = yRange.reduce(
+                (acc2, y) => {
+                    if (checkMove(board, x, y) === false) {
+                        return acc2;
+                    } else {
+                        const nb: Board = placeTile(board, tile, x, y);
+                        const rg: G.Graph<Quarter> = buildRoadGraph(board);
+                        const ng: G.Graph<Quarter> = buildNeighborhoodGraph(board);
 
-			const score = O.objectives_player_gain(ng, rg, nb, objectives);
-			//console.log("Score for coordinates (" + x + ", " + y + "): " + score);
+                        const score = O.objectives_player_gain(ng, rg, nb, objectives);
+                        //console.log("Score for coordinates (" + x + ", " + y + "): " + score);
 
-			//console.log("acc2: ", acc2.toArray());
+                        //console.log("acc2: ", acc2.toArray());
 
-			if (score > acc2.get(1, -Infinity)) {
-			    //console.log("New best Y score found!");
-			    const nacc = acc2.set(1, score).set(0, y);
-			    //console.log("nacc2:", nacc.toArray());
-			    return nacc;
-			}
-			return acc2;
-		    }
-		},
-		List([0, -Infinity])
-	    )
-	    //console.log("bestY:", bestY.toArray());
-	    //console.log("acc:", acc.toArray());
-	    if (bestY.get(1, -Infinity) > acc.get(2, -Infinity)) {
-		//console.log("New best X score found!");
-		const nacc = acc.set(2, bestY.get(1, -Infinity)).set(1, bestY.get(0, 0)).set(0, x);
-		//console.log("nacc", nacc.toArray());
-		return nacc;
-	    } else {
-		return acc;
-	    }
-	},
-	List([0, 0, -Infinity])
+                        if (score > acc2.get(1, -Infinity)) {
+                            //console.log("New best Y score found!");
+                            const nacc = acc2.set(1, score).set(0, y);
+                            //console.log("nacc2:", nacc.toArray());
+                            return nacc;
+                        }
+                        return acc2;
+                    }
+                },
+                List([0, -Infinity])
+            );
+            //console.log("bestY:", bestY.toArray());
+            //console.log("acc:", acc.toArray());
+            if (bestY.get(1, -Infinity) > acc.get(2, -Infinity)) {
+                //console.log("New best X score found!");
+                const nacc = acc.set(2, bestY.get(1, -Infinity)).set(1, bestY.get(0, 0)).set(0, x);
+                //console.log("nacc", nacc.toArray());
+                return nacc;
+            } else {
+                return acc;
+            }
+        },
+        List([0, 0, -Infinity])
     );
 
     return bestCoords;

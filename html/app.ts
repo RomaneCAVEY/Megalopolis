c503369c1dba24b8de835e5dc3b98fa6e4d95e72
Faustine Lacroix <faustine.lacroix@enseirb-matmeca.fr>
2024-05-07 17:22:12 +0200

    correcrt , issue and tabify issus

--- html/app.ts
+++ html/app.ts
@@ -1,22 +1,22 @@
 import * as I from "../src/interactive.ts";
-import * as B from "../src/board.ts"
+import * as B from "../src/board.ts";
 import * as C from "../src/neighborhood.ts";
 import * as Disp from "../src/display.ts";
 
-let statusText = document.getElementById("status");
+const statusText = document.getElementById("status");
 
-let gameStates: I.GameState = [];
+const gameStates: I.GameState = [];
 
 document.getElementById("launch-button").addEventListener('click', () => {
     statusText.innerText = "Calcul en cours...";
     if (gameStates.length === 0) {
-	gameStates.push(I.initGame(10));
-	console.log(gameStates.slice(-1)[0]);
-	statusText.innerText = "Partie initialisée ! Tour " + gameStates.slice(-1)[0].get('turn');
-	document.getElementById("launch-button").innerText = "Tour suivant";
+        gameStates.push(I.initGame(10));
+        console.log(gameStates.slice(-1)[0]);
+        statusText.innerText = "Partie initialisée ! Tour " + gameStates.slice(-1)[0].get('turn');
+        document.getElementById("launch-button").innerText = "Tour suivant";
     } else {
-	gameStates.push(I.playTurn(gameStates.slice(-1)[0]));
-	statusText.innerText = "Tour " + gameStates.slice(-1)[0].get('turn');
+        gameStates.push(I.playTurn(gameStates.slice(-1)[0]));
+        statusText.innerText = "Tour " + gameStates.slice(-1)[0].get('turn');
     }
     document.getElementById("board").innerHTML = makeBoard(gameStates.slice(-1)[0]);
     document.getElementById("score").innerText = I.score(gameStates.slice(-1)[0]);
@@ -30,7 +30,7 @@ document.getElementById("display-board-term").addEventListener('click', () => {
 
 function makeBoard(gameState: I.GameState) {
     return gameState.get('board').reduce((acc, e) => {
-	return acc + `<div class="tile" style="grid-row: ` + (15 - e.get('y')) + `; grid-column: ` + (e.get('x') + 15) + `;">` + makeQuarter(e) + `</div>`;
+        return acc + `<div class="tile" style="grid-row: ` + (15 - e.get('y')) + `; grid-column: ` + (e.get('x') + 15) + `;">` + makeQuarter(e) + `</div>`;
     }, "");
 }
 
@@ -41,14 +41,14 @@ function makeQuarter(quarter: B.Quarter) {
 
     // Draw roads
     if (quarter.get("road").get("north"))
-	svg += `<line x1="2" y1="0" x2="2" y2="2" stroke="#BBBBBB" stroke-linecap="round" />`;
+        svg += `<line x1="2" y1="0" x2="2" y2="2" stroke="#BBBBBB" stroke-linecap="round" />`;
     if (quarter.get("road").get("east"))
-	svg += `<line x1="2" y1="2" x2="4" y2="2" stroke="#BBBBBB" stroke-linecap="round" />`;
+        svg += `<line x1="2" y1="2" x2="4" y2="2" stroke="#BBBBBB" stroke-linecap="round" />`;
     if (quarter.get("road").get("south"))
-	svg += `<line x1="2" y1="2" x2="2" y2="4" stroke="#BBBBBB" stroke-linecap="round" />`;
+        svg += `<line x1="2" y1="2" x2="2" y2="4" stroke="#BBBBBB" stroke-linecap="round" />`;
 
     if (quarter.get("road").get("west"))
-	svg += `<line x1="0" y1="2" x2="2" y2="2" stroke="#BBBBBB" stroke-linecap="round" />`;
+        svg += `<line x1="0" y1="2" x2="2" y2="2" stroke="#BBBBBB" stroke-linecap="round" />`;
 
     svg += `</svg>`;
     
@@ -57,20 +57,20 @@ function makeQuarter(quarter: B.Quarter) {
 
 function getDisplayColor(c: C.Color): string {
     switch (c) {
-	case C.Color.Green:
-	return "#00DFA2";
+        case C.Color.Green:
+            return "#00DFA2";
 
-	case C.Color.Blue:
-	return "#0079FF";
+        case C.Color.Blue:
+            return "#0079FF";
 
-	case C.Color.Red:
-	return "#FF0060";
+        case C.Color.Red:
+            return "#FF0060";
 
-	case C.Color.Grey:
-	return "#666666";
+        case C.Color.Grey:
+            return "#666666";
 
-	default:
-	return "#FF00FF";
+        default:
+            return "#FF00FF";
     }
 }
 
--- html/script.ts
+++ html/script.ts
@@ -9,35 +9,35 @@ const board=finalBoard;
 
 // Tableau d'URLs d'images
 const images = [
-	"empty.png", 
-	"green.png", 
-	"o_c.png", 
-	"o_h.pgn",
-	"o_v.png",
-	"b_c.png", 
-	"b_h.pgn",
-	"b_v.png",
-	"i_c.png", 
-	"i_h.pgn",
-	"i_v.png"
+    "empty.png", 
+    "green.png", 
+    "o_c.png", 
+    "o_h.pgn",
+    "o_v.png",
+    "b_c.png", 
+    "b_h.pgn",
+    "b_v.png",
+    "i_c.png", 
+    "i_h.pgn",
+    "i_v.png"
 ];
 
 
 
 function quarter_line_in_board(board: B.Board, i:number){
-	return board.filter((e)=> e!== undefined && e.get('x')===i	);	
+    return board.filter((e)=> e!== undefined && e.get('x')===i  );      
 }
 
 
 function display(){
-	function bis(i:number){
-		if (i===(15*2)){
-			return ;
-		}
-		quarter_line_in_board(finalBoard,i);
-		bis(i++);
-	}
-	return bis(-15*2);
+    function bis(i:number){
+        if (i===(15*2)){
+            return ;
+        }
+        quarter_line_in_board(finalBoard,i);
+        bis(i++);
+    }
+    return bis(-15*2);
 }
 
 
@@ -50,102 +50,102 @@ function display(){
 function correspondante(quartier : B.Quarter): any //List<number>
 {
 
-	if (quartier===undefined){
-		return List<number>().push(0).push(0);
-	}
-	if (quartier.get('color')===C.Color.Green){
-		return List<number>().push(1).push(0);
-	}
+    if (quartier===undefined){
+        return List<number>().push(0).push(0);
+    }
+    if (quartier.get('color')===C.Color.Green){
+        return List<number>().push(1).push(0);
+    }
 
-	//Check blue
-	if (quartier.get('color')===C.Color.Blue){
-		if (quartier.get('road').get('east')===true && quartier.get('road').get('west')===true ){
-			return List<number>().push(6).push(0);
-		}
-		if (quartier.get('road').get('south')===true && quartier.get('road').get('north')===true ){
-			return List<number>().push(7).push(0);
-		}
-		if (quartier.get('road').get('north')===true && quartier.get('road').get('east')===true ){
-			return List<number>().push(5).push(0) ;
-		}
-		if (quartier.get('road').get('east')===true && quartier.get('road').get('south')===true ){
-			
-			return List<number>().push(5).push(90);
-		}
-		if (quartier.get('road').get('south')===true && quartier.get('road').get('west')===true ){
-			return List<number>().push(5).push(180);
+    //Check blue
+    if (quartier.get('color')===C.Color.Blue){
+        if (quartier.get('road').get('east')===true && quartier.get('road').get('west')===true ){
+            return List<number>().push(6).push(0);
+        }
+        if (quartier.get('road').get('south')===true && quartier.get('road').get('north')===true ){
+            return List<number>().push(7).push(0);
+        }
+        if (quartier.get('road').get('north')===true && quartier.get('road').get('east')===true ){
+            return List<number>().push(5).push(0) ;
+        }
+        if (quartier.get('road').get('east')===true && quartier.get('road').get('south')===true ){
+            
+            return List<number>().push(5).push(90);
+        }
+        if (quartier.get('road').get('south')===true && quartier.get('road').get('west')===true ){
+            return List<number>().push(5).push(180);
 
-		}
-		if (quartier.get('road').get('north')===true && quartier.get('road').get('west')===true ){
-			return List<number>().push(5).push(270);
+        }
+        if (quartier.get('road').get('north')===true && quartier.get('road').get('west')===true ){
+            return List<number>().push(5).push(270);
 
-		}
-	}
-		//check red
-		if (quartier.get('color')===C.Color.Red){
-			if (quartier.get('road').get('east')===true && quartier.get('road').get('west')===true ){
-				return List<number>().push(3).push(0);
-			}
-			if (quartier.get('road').get('south')===true && quartier.get('road').get('north')===true ){
-				return List<number>().push(4).push(0);
-			}
-			if (quartier.get('road').get('north')===true && quartier.get('road').get('east')===true ){
-				return List<number>().push(2).push(0) ;
-			}
-			if (quartier.get('road').get('east')===true && quartier.get('road').get('south')===true ){
-				
-				return List<number>().push(2).push(90);
-			}
-			if (quartier.get('road').get('south')===true && quartier.get('road').get('west')===true ){
-				return List<number>().push(2).push(180);
-	
-			}
-			if (quartier.get('road').get('north')===true && quartier.get('road').get('west')===true ){
-				return List<number>().push(2).push(270);
-	
-			}
-		}
+        }
+    }
+    //check red
+    if (quartier.get('color')===C.Color.Red){
+        if (quartier.get('road').get('east')===true && quartier.get('road').get('west')===true ){
+            return List<number>().push(3).push(0);
+        }
+        if (quartier.get('road').get('south')===true && quartier.get('road').get('north')===true ){
+            return List<number>().push(4).push(0);
+        }
+        if (quartier.get('road').get('north')===true && quartier.get('road').get('east')===true ){
+            return List<number>().push(2).push(0) ;
+        }
+        if (quartier.get('road').get('east')===true && quartier.get('road').get('south')===true ){
+            
+            return List<number>().push(2).push(90);
+        }
+        if (quartier.get('road').get('south')===true && quartier.get('road').get('west')===true ){
+            return List<number>().push(2).push(180);
+            
+        }
+        if (quartier.get('road').get('north')===true && quartier.get('road').get('west')===true ){
+            return List<number>().push(2).push(270);
+            
+        }
+    }
 
 
-		//check grey
-		if (quartier.get('color')===C.Color.Grey){
-			if (quartier.get('road').get('east')===true && quartier.get('road').get('west')===true ){
-				return List<number>().push(9).push(0);
-			}
-			if (quartier.get('road').get('south')===true && quartier.get('road').get('north')===true ){
-				return List<number>().push(10).push(0);
-			}
-			if (quartier.get('road').get('north')===true && quartier.get('road').get('east')===true ){
-				return List<number>().push(8).push(0) ;
-			}
-			if (quartier.get('road').get('east')===true && quartier.get('road').get('south')===true ){
-				
-				return List<number>().push(8).push(90);
-			}
-			if (quartier.get('road').get('south')===true && quartier.get('road').get('west')===true ){
-				return List<number>().push(8).push(180);
-	
-			}
-			if (quartier.get('road').get('north')===true && quartier.get('road').get('west')===true ){
-				return List<number>().push(8).push(270);
-	
-			}
-		}
-		return 0;
-		
+    //check grey
+    if (quartier.get('color')===C.Color.Grey){
+        if (quartier.get('road').get('east')===true && quartier.get('road').get('west')===true ){
+            return List<number>().push(9).push(0);
+        }
+        if (quartier.get('road').get('south')===true && quartier.get('road').get('north')===true ){
+            return List<number>().push(10).push(0);
+        }
+        if (quartier.get('road').get('north')===true && quartier.get('road').get('east')===true ){
+            return List<number>().push(8).push(0) ;
+        }
+        if (quartier.get('road').get('east')===true && quartier.get('road').get('south')===true ){
+            
+            return List<number>().push(8).push(90);
+        }
+        if (quartier.get('road').get('south')===true && quartier.get('road').get('west')===true ){
+            return List<number>().push(8).push(180);
+            
+        }
+        if (quartier.get('road').get('north')===true && quartier.get('road').get('west')===true ){
+            return List<number>().push(8).push(270);
+            
+        }
+    }
+    return 0;
+    
 }
 
 
 /* function creerMatrice(i, j) {
-var matrice = [];
-for (var row = 0; row < i; row++) {
-	matrice[row]=[];
-	for (var col = 0; col < j; col++) {
-		matrice[row][col] = images[0];; // Vous pouvez initialiser les valeurs comme vous le souhaitez
-	}
-}
-return matrice;
-} */
+   var matrice = [];
+   for (var row = 0; row < i; row++) {
+   matrice[row]=[];
+   for (var col = 0; col < j; col++) {
+   matrice[row][col] = images[0];; // Vous pouvez initialiser les valeurs comme vous le souhaitez
+   }
+   }
+   return matrice;
+   } */
 
 /* 
 // Fonction pour créer et afficher les balises img
@@ -163,12 +163,12 @@ if(imageTableDiv)
 imageTableDiv.appendChild(img);
 else
 {
-console	.log("cou");
+console .log("cou");
 }
 });
 }
 
 // Appel de la fonction pour afficher les images
 afficherImages();
- */
+*/
 display(); 
